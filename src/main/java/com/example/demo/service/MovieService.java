3cb910bd3a47c1f317bc23af214ce2e6cb81aa7f
Dmitrii_Dementev <dmitrii_dementev@epam.com>
2019-07-18 19:11:57 +0300

    added logging example

--- src/main/java/com/example/demo/service/MovieService.java
+++ src/main/java/com/example/demo/service/MovieService.java
@@ -3,6 +3,8 @@ package com.example.demo.service;
 import com.example.demo.dto.MovieDto;
 import com.example.demo.repository.MovieRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,10 +13,16 @@ import java.util.stream.Collectors;
 @Service
 @AllArgsConstructor
 public class MovieService {
+    private static final Logger LOG = LoggerFactory.getLogger(MovieService.class);
 
     private final MovieRepository movieRepository;
 
     public List<MovieDto> getAllMovies() {
+        LOG.trace("trace");
+        LOG.debug("debug");
+        LOG.info("info");
+        LOG.warn("warn");
+        LOG.error("error");
         return movieRepository.findAll().stream()
                 .map(entity -> new MovieDto(entity.getId(), entity.getName()))
                 .collect(Collectors.toList());
